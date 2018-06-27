24891f030a19ae3580ca926bc7945d62fa70a111
Hayden Betts <haydenbetts@gmail.com>
2018-06-27 20:42:42 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -35,11 +35,11 @@ class Song
     end
 
     def self.artists
-      @@artists
+      @@artists.uniq
     end
 
     def self.genres
-      @@genres
+      @@genres.uniq
     end
 
     def self.artist_count
